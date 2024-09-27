aca2bccec73d74a1fe637c6818ce6848e118ce26
Atticuszz <1831768457@qq.com>
2024-09-27 18:58:17 +0800

    pre-commit

--- src/main.py
+++ src/main.py
@@ -1,16 +1,11 @@
-
-
-
-
 class Dog:
-    
     def __init__(self) -> None:
-        self.age=0
+        self.age = 0
+
 
 def main():
     print("hello world!")
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
